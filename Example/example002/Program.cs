9ea772ae397108e86abd13de584700a5cf15cca2
Kirill Kurbatov <kirillkurbatov482.com>
2022-12-25 21:49:58 +0300

    решил задачи и записал решения для второго семинара

--- Example/example002/Program.cs
+++ Example/example002/Program.cs
@@ -1,19 +1,36 @@
-﻿Console.Clear();
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > -100 && number < 100)
-{
-    Console.WriteLine($"Третьей цифры нет");
-}
-else
-{
-    while (number <= -1000 || number >= 1000)
-    {
-        number /= 10;
-    }
-    int secondnumber = number %10;
-    Console.WriteLine($"Третья цифра числа -> {secondnumber}");
-}
+﻿// 10 задача, второй семинар
+// int number = new Random().Next(100, 1000);
+// int secondnumber = number / 10;
+// int lastNum = secondnumber % 10;
+// Console.WriteLine(number);
+// Console.WriteLine(lastNum);
+
+// // 13 задача, второй семинар
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > -100 && number < 100)
+// {
+//     Console.WriteLine($"Третьей цифры нет");
+// }
+// else
+// {
+//     while (number <= -1000 || number >= 1000)
+//     {
+//         number /= 10;
+//     }
+//     int secondnumber = number %10;
+//     Console.WriteLine($"Третья цифра числа -> {secondnumber}");
+// }
+
+
+// 15 задача, второй семинар
+// int number = new Random().Next(1, 8);
+// Console.WriteLine (number);
+// if (number > 6)
+// Console.WriteLine($"Является выходным");
+// else 
+// Console.WriteLine($"Не является выходным");
 
 
 
