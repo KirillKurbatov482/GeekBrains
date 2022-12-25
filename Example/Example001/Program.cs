88f9047eb33c163131f9d44a44bac6c95769622d
Kirill Kurbatov <kirillkurbatov482.com>
2022-12-25 17:40:51 +0300

    добавил решение задач от первого семинара

--- Example/Example001/Program.cs
+++ Example/Example001/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// # // 2 задача, первый семинар
+
+// Console.Write($"Введите первое число: ");
+// float numFirst = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write($"Введите второе число: ");
+// float numSecond = Convert.ToInt32(Console.ReadLine());
+
+// if ((numFirst > numSecond))
+// Console.WriteLine(numFirst);
+// else 
+// Console.WriteLine(numSecond);
+
+
+
+// # // 4 задача, первый семинар
+
+// Console.Write($"Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write($"Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write($"Введите второе число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if ((a > b) & (a > c))
+// Console.WriteLine(a);
+// else if
+// (b > c)
+// Console.WriteLine(b);
+// else 
+// Console.WriteLine(c);
+
+
+// # // 6 задача, первый семинар 
+
+// Console.Write($"Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if  (num%2 == 0)  {
+// Console.WriteLine($"Четное");
+// }
+// else {
+// Console.WriteLine($"Не четное");
+// }
+
+// # // 8 задача, первый семинар 
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i++)
+// if (i % 2 == 0)
+// Console.WriteLine(i);
