957270067aad6e11ea426287b34a91441f352fe5
Kirill Kurbatov <kirillkurbatov482.com>
2022-12-25 20:22:58 +0300

    решил задачи для второго семинара

--- Example/example002/Program.cs
+++ Example/example002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > -100 && number < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+else
+{
+    while (number <= -1000 || number >= 1000)
+    {
+        number /= 10;
+    }
+    int secondnumber = number %10;
+    Console.WriteLine($"Третья цифра числа -> {secondnumber}");
+}
+
+
+
