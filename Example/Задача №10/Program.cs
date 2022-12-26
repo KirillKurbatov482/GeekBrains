3e37322fd9ccf0669083873472de73836517f015
Kirill Kurbatov <kirillkurbatov482.com>
2022-12-26 23:18:12 +0300

    Создал отдельные cs. файлы для каждой задачи

---
+++ Example/Задача №10/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = new Random().Next(100, 1000);
+int secondnumber = number / 10;
+int lastNum = secondnumber % 10;
+Console.WriteLine(number);
+Console.WriteLine(lastNum);
---
+++ Example/Задача №15/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(1, 8);
+Console.WriteLine (number);
+if (number > 6)
+Console.WriteLine($"Является выходным");
+else 
+Console.WriteLine($"Не является выходным");
