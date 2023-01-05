d34c29fe04d2b54c8d1de7a08a72c7352cc3355e
Kirill Kurbatov <kirillkurbatov482.com>
2023-01-05 12:19:48 +0300

    провёл работу над ошибками

--- Example/003 №21/Program.cs
+++ Example/003 №21/Program.cs
@@ -6,10 +6,14 @@ Console.Write("Введите X1: ");
 int x1=int.Parse(Console.ReadLine());
 Console.Write("Введите Y1: ");
 int y1=int.Parse(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z1=int.Parse(Console.ReadLine());
 Console.Write("Введите X2: ");
 int x2=int.Parse(Console.ReadLine());
 Console.Write("Введите Y2: ");
 int y2=int.Parse(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z2=int.Parse(Console.ReadLine());
 
-double d = Math.Sqrt(Math.Pow(x1-x2, 3) + Math.Pow(y1-y2, 3));
+double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
 Console.WriteLine($"d={d:f3}");
